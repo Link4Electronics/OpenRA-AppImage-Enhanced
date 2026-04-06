d3c2cb39e988431e77dd27913e6cfbe19fbd4bbd
Link4Electronics <link4electronics@gmail.com>
2026-04-06 15:42:27 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -8,14 +8,14 @@ echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
     libdecor \
-    openra
+    mono
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+make-aur-package openra-git
 
 # If the application needs to be manually built that has to be done down here
 
