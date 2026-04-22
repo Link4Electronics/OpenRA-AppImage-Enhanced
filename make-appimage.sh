ccc94a6090ba83998a9f9461ec9482b144ec4d77
Link4Electronics <link4electronics@gmail.com>
2026-04-22 04:17:41 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -15,10 +15,7 @@ export DEPLOY_OPENGL=1
 export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
-mkdir -p ./AppDir/bin
-cp -r /usr/lib/openra/* ./AppDir/bin
-#quick-sharun /usr/bin/openra-* /usr/lib/openra/*
-quick-sharun /usr/bin/openra-* ./AppDir/bin/*
+quick-sharun /usr/bin/openra-* /usr/lib/openra
 
 # Additional changes can be done in between here
 
